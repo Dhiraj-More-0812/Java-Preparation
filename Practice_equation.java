695b8d8d97bd8d439b496dfbe1dd9f1b17a24389
Dhiraj More <dhirajmore0812@gmail.com>
2024-06-20 21:18:59 +0530

    Add files via upload

---
+++ Practice_equation.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Practice_equation {
+    public static void main(String[] args){
+        int x = 7;
+        int total = ++x * 8;
+        System.out.println(total);
+        char a='b';
+        System.out.println(++a);
+        float f = 7/4f * 9/2f;
+        System.out.println(f);
+        double f3 = 7/4f;
+        System.out.println(f3);
+        float f4 = 9/2;
+        double f5;
+        System.out.println(f5 = f3 * f4);
+        int x1 = 7;
+        int a1 = 7*49/7 + 35/7;
+        System.out.println(a1);
+
+    }
+}
