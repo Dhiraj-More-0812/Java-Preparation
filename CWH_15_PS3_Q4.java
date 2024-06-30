7db5bf923da62d1892a6a42847f81cd4ba4eeb4c
Dhiraj More <dhirajmore0812@gmail.com>
2024-06-30 21:36:02 +0530

    Add files via upload

---
+++ CWH_15_PS3_Q4.java
@@ -0,0 +1,7 @@
+public class CWH_15_PS3_Q4 {
+    public static void main(String[] args){
+        String String1 = "This   string    has   some spaces   ";
+        System.out.println(String1.indexOf("   "));
+        System.out.println(String1.indexOf("    "));
+    }
+}
---
+++ CWH_15_PS3_Q5.java
@@ -0,0 +1,7 @@
+public class CWH_15_PS3_Q5 {
+    public static void main(String[] args) {
+        String letter = "Dear Harry,\n \tThis java course is nice.\n \tThanks";
+        System.out.println(letter);
+
+    }
+}
