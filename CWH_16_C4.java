f18c9d24560f65d3d82eadc29ea8a29eff105796
Dhiraj More <dhirajmore0812@gmail.com>
2024-07-03 21:18:40 +0530

    Add files via upload

---
+++ CWH_16_C4.java
@@ -0,0 +1,18 @@
+public class CWH_16_C4 {
+    public static void main(String[] args) {
+        int age = 27;
+        if(age>18){
+            System.out.println("You can drive");
+        }
+        else{
+            System.out.println("You can't drive");
+        }
+        boolean cond = (age == 27);
+        if(cond){
+            System.out.println("You can drive");
+        }
+        else{
+            System.out.println("You can't drive");
+        }
+    }
+}
